8ad4b789ec7a9a6a57e237adb43d4000dc4b812f
Dan-Ukendi <dan.ukendi1@gmail.com>
2026-06-11 13:01:08 +0200

    final commit : top10 + merged functiun

--- Team 1/Exercise 1/data_manip.py
+++ Team 1/Exercise 1/data_manip.py
@@ -17,3 +17,21 @@ top_10_skills = (skills_merged
 print(top_10_skills)
 print(postings_fact.describe())
 print(postings_fact.info())
+
+def name_to_jobid(postings,skilljob,skilldim):
+    job_id = postings.query("job_title_short == 'Data Engineer'")['job_id'].tolist()
+    filtered = skilljob[skilljob['job_id'].isin(job_id)]
+    merged = filtered.merge(skilldim,on = 'skill_id')
+    return merged
+def top_10(merged_frame):
+    top_10_skills = (merged_frame
+                 .groupby('skills')['skill_id']
+                 .count()
+                 .reset_index()
+                 .rename(columns={'skill_id': 'demand_count'})
+                 .sort_values('demand_count', ascending=False)
+                 .head(10))
+    return top_10_skills
+frame = name_to_jobid(postings_fact,skills_job,skills_dim)
+top10 = top_10(frame)
+print(top10)
